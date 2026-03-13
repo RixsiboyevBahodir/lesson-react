68ef37177cc8bb18c2dd78fb3b505fe808c0f8fa
Iqboljon-Web-Developer-7 <iqboljonummataliyev30@gmail.com>
2026-03-13 20:44:54 +0500

    style(CarouselImg): standardize quotes and remove unused image import
    - Use double quotes consistently for imports and string literals
    - Remove unused 'hobbi' image import to clean up code
    - Adjust indentation and formatting for better readability

--- src/components/ui/CarouselImg.jsx
+++ src/components/ui/CarouselImg.jsx
@@ -1,36 +1,31 @@
-import { Carousel } from 'antd';
-import mart from '../../assets/mart8.jpg'
-import phone from '../../assets/phone.jpg'
-import gift from '../../assets/gift.jpg'
-import hobbi from '../../assets/forhobbi.jpg'
-import discount from '../../assets/discount.jpg'
+import { Carousel } from "antd";
+import mart from "../../assets/mart8.jpg";
+import phone from "../../assets/phone.jpg";
+import gift from "../../assets/gift.jpg";
+import discount from "../../assets/discount.jpg";
 
 export default function CarouselImg() {
+  const contentStyle = {
+    height: "400px",
+    borderRadius: "12px",
+  };
 
-    const contentStyle = {
-        height: '400px',
-        borderRadius: "12px",
-    };
-
-    return (
+  return (
+    <div>
+      <Carousel arrows dotPlacement="left" autoplay={{ dotDuration: true }}>
         <div>
-            <Carousel arrows dotPlacement="left" autoplay={{ dotDuration: true }}>
-                <div>
-                    <img src={mart} alt="" style={contentStyle} />
-                </div>
-                <div>
-                    <img src={phone} alt="" style={contentStyle} />
-                </div>
-                <div>
-                    <img src={gift} alt="" style={contentStyle} />
-                </div>
-                <div>
-                    <img src={hobbi} alt="" style={contentStyle} />
-                </div>
-                <div>
-                    <img src={discount} alt="" style={contentStyle} />
-                </div>
-            </Carousel>
+          <img src={mart} alt="" style={contentStyle} />
         </div>
-    )
+        <div>
+          <img src={phone} alt="" style={contentStyle} />
+        </div>
+        <div>
+          <img src={gift} alt="" style={contentStyle} />
+        </div>
+        <div>
+          <img src={discount} alt="" style={contentStyle} />
+        </div>
+      </Carousel>
+    </div>
+  );
 }
